ec00e3341a1c50b3340addebb0316b1272c13ef6
JacobJB08 <116194207+JacobJB08@users.noreply.github.com>
2024-01-24 13:48:29 -0800

    fya

---
+++ ScriptingProjectBonilla/Assets/GamePro2/Scripts/Statics/Game1.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game1 : MonoBehaviour
+{
+    private void Start()
+    {
+        Enemy1 enemy1 = new Enemy1();
+        Enemy1 enemy2 = new Enemy1();
+        Enemy1 enemy3 = new Enemy1();
+
+        int x = Enemy1.enemyCount;
+    }
+}
