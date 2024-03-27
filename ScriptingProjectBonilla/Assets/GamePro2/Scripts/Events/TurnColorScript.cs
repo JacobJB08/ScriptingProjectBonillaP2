3ef7aa9d630a9a9d91910ba1740a7d7faeae31d1
JacobJB08 <116194207+JacobJB08@users.noreply.github.com>
2024-03-27 12:30:16 -0700

    www

---
+++ ScriptingProjectBonilla/Assets/GamePro2/Scripts/Events/TurnColorScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnColorScript : MonoBehaviour
+{
+    void OnEnable()
+    {
+        EventManager.OnClicked += TurnColor;
+    }
+
+
+    void OnDisable()
+    {
+        EventManager.OnClicked -= TurnColor;
+    }
+
+
+    void TurnColor()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        Color col = new Color(Random.value, Random.value, Random.value);
+        renderer.material.color = col;
+    }
+}
